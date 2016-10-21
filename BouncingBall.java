ffff3ee3fc5b2ae516c81210265c931a3e1519c2
DucPham <zea_0107>
2016-10-21 16:28:27 +0700

    add sum left right close #2

--- BouncingBall.java
+++ BouncingBall.java
@@ -5,11 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
-import java.awt.image.BufferedImage;
-import java.util.Date;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,6 +16,7 @@ import javax.swing.Timer;
  * @author Huyen
  *
  */
+@SuppressWarnings("serial")
 public class BouncingBall extends JFrame {
 	private static final int WIDTH = 400;
 	private static final int HEIGHT = 400;
@@ -39,7 +36,8 @@ public class BouncingBall extends JFrame {
 	int rightPaddleHeight = 100;
 	//Mouse
 	int mouseX = 0, mouseY =0;
-	
+	int counleft =0,counright=0;
+
 	/**
 	 * Constructor
 	 */
@@ -66,12 +64,18 @@ public class BouncingBall extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//Update ball position
+
 				x += xSpeed;
 				y += ySpeed;
 				//Reverse the direction of the ball if it hits the left or right wall
 				if (x < 0 || x > WIDTH - size) {
 					xSpeed = -xSpeed;
 					changeBallColor();
+				if(x>WIDTH - size){counleft ++;
+				}
+				if(x<0){counright ++;
+				}
+
 				}
 				//Reverse the direction of the ball if it hits the top or bottom wall
 				if (y < 0 || y > HEIGHT - size) {
@@ -128,7 +132,9 @@ public class BouncingBall extends JFrame {
 			g.setColor(Color.BLACK);
 			int leftPaddleX = 0;
 			g.fillRect(leftPaddleX, leftPaddleY, paddleWidth, leftPaddleHeight);
+			g.drawString(""+counleft,370,20);
 			//Right Paddle
+			g.drawString(""+counright,20,20);
 			int rightPaddleX = getWidth()-paddleWidth;
 			g.fillRect(rightPaddleX, rightPaddleY, paddleWidth, rightPaddleHeight);
 			
