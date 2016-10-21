064b03f22525d4502cb52fb81e1c6b62dc9cfa1c
tongoclam <tongoclam1@gmail.com>
2016-10-21 16:22:44 +0700

    Closed #1 control 2 paddles

--- BouncingBall.java
+++ BouncingBall.java
@@ -3,6 +3,9 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseMotionListener;
@@ -18,28 +21,29 @@ import javax.swing.Timer;
 
 /**
  * @author Huyen
- *
+ * 
  */
 public class BouncingBall extends JFrame {
 	private static final int WIDTH = 400;
 	private static final int HEIGHT = 400;
 	private static final int UPDATETIME = 5;
 	private DrawingCanvas canvas;
-	//Ball
+	// Ball
 	int x = 50, y = 50;
 	int size = 30;
 	int xSpeed = 1, ySpeed = 2;
-	Color[] ballColorArray = { Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.GRAY, Color.PINK, Color.BLACK };
+	Color[] ballColorArray = { Color.RED, Color.GREEN, Color.BLUE,
+			Color.YELLOW, Color.GRAY, Color.PINK, Color.BLACK };
 	Color ballColor = Color.BLACK;
-	//Paddle
+	// Paddle
 	int paddleWidth = 10;
 	int leftPaddleY;
 	int leftPaddleHeight = 100;
 	int rightPaddleY;
 	int rightPaddleHeight = 100;
-	//Mouse
-	int mouseX = 0, mouseY =0;
-	
+	// Mouse
+	int mouseX = 0, mouseY = 0;
+
 	/**
 	 * Constructor
 	 */
@@ -53,11 +57,24 @@ public class BouncingBall extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		this.addMouseMotionListener(new MouseMotionAdapter() {
-			
+
 			@Override
 			public void mouseMoved(MouseEvent e) {
 				mouseX = e.getX();
 				mouseY = e.getY();
+			}
+		});
+		this.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					rightPaddleY -= 10;
+				} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					rightPaddleY += 10;
+				} else if (e.getKeyCode() == KeyEvent.VK_W) {
+					leftPaddleY -= 10;
+				} else if (e.getKeyCode() == KeyEvent.VK_S) {
+					leftPaddleY += 10;
+				}
 			}
 		});
 
@@ -65,26 +82,28 @@ public class BouncingBall extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//Update ball position
+				// Update ball position
 				x += xSpeed;
 				y += ySpeed;
-				//Reverse the direction of the ball if it hits the left or right wall
+				// Reverse the direction of the ball if it hits the left or
+				// right wall
 				if (x < 0 || x > WIDTH - size) {
 					xSpeed = -xSpeed;
 					changeBallColor();
 				}
-				//Reverse the direction of the ball if it hits the top or bottom wall
+				// Reverse the direction of the ball if it hits the top or
+				// bottom wall
 				if (y < 0 || y > HEIGHT - size) {
 					ySpeed = -ySpeed;
 					changeBallColor();
 				}
-				//Set fixed paddles position
-				leftPaddleY = (getHeight()-40)/2 - leftPaddleHeight/2;
-				rightPaddleY = (getHeight()-40)/2 - rightPaddleHeight/2;
+				// Set fixed paddles position
+				// leftPaddleY = (getHeight()-40)/2 - leftPaddleHeight/2;
+				// rightPaddleY = (getHeight()-40)/2 - rightPaddleHeight/2;
 				repaint();
 			}
 		};
-		//Run timer
+		// Run timer
 		Timer timer = new Timer(UPDATETIME, updateTask);
 		timer.start();
 	}
@@ -109,32 +128,34 @@ public class BouncingBall extends JFrame {
 		});
 
 	}
-	//Nested Class (Class bên trong class khác)
+
+	// Nested Class (Class bên trong class khác)
 	private class DrawingCanvas extends JPanel {
 
 		@Override
 		protected void paintComponent(Graphics g) {
 			long start = System.nanoTime();
 			super.paintComponent(g);
-			setBackground(new Color(175,215,230));
-			//Ball
+			setBackground(new Color(175, 215, 230));
+			// Ball
 			g.setColor(ballColor);
 			g.fillOval(x, y, size, size);
-			//Mouse trail
+			// Mouse trail
 			g.setColor(Color.RED);
-			g.fillOval(mouseX-10, mouseY-10, 20, 20);
-			
-			//Left Paddle
+			g.fillOval(mouseX - 10, mouseY - 10, 20, 20);
+
+			// Left Paddle
 			g.setColor(Color.BLACK);
 			int leftPaddleX = 0;
 			g.fillRect(leftPaddleX, leftPaddleY, paddleWidth, leftPaddleHeight);
-			//Right Paddle
-			int rightPaddleX = getWidth()-paddleWidth;
-			g.fillRect(rightPaddleX, rightPaddleY, paddleWidth, rightPaddleHeight);
-			
-			//Debug
+			// Right Paddle
+			int rightPaddleX = getWidth() - paddleWidth;
+			g.fillRect(rightPaddleX, rightPaddleY, paddleWidth,
+					rightPaddleHeight);
+
+			// Debug
 			long duration = System.nanoTime() - start;
-			//System.out.println("Paint in : "+ duration+ " nano seconds");
+			// System.out.println("Paint in : "+ duration+ " nano seconds");
 		}
 	}
 }
